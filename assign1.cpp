40769902eaf8241df15ebf4af56485f1c77e0c86
Tanushka Patil <94859584+tanushkapatil@users.noreply.github.com>
2025-01-08 16:13:09 +0530

    Update assign1.cpp

--- assign1.cpp
+++ assign1.cpp
@@ -9,3 +9,38 @@ class TreeNode {
         TreeNode(int value) : data(value), left(nullptr), right(nullptr) {}
 };
 
+class Tree {
+    public:
+        TreeNode *root;
+        Tree(); 
+        void insert(int value);
+        void insert(TreeNode*& root, int value); // Recursive helper function
+        void inOrder(TreeNode* root);
+};
+
+Tree::Tree() { 
+    root = nullptr; 
+}
+
+void Tree::insert(TreeNode*& root, int value) {
+    if (root == nullptr) {
+        root = new TreeNode(value);
+    } else if (value < root->data) {
+        insert(root->left, value);
+    } else {
+        insert(root->right, value);
+    }
+}
+
+void Tree::insert(int value) {
+    insert(root, value);
+}
+
+void Tree::inOrder(TreeNode* root) {
+    if (root != nullptr) {
+        inOrder(root->left);
+        cout << root->data << " ";
+        inOrder(root->right);
+    }
+}
+
