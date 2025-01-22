1b2f1c47b9f1c26706efa819f806403c13d21c03
Tanushka Patil <94859584+tanushkapatil@users.noreply.github.com>
2025-01-22 15:59:46 +0530

    Update assign1.cpp

--- assign1.cpp
+++ assign1.cpp
@@ -1,26 +1,42 @@
-#include<iostream>
+// create
+// insert
+// traversal:
+    // inorder
+    // preorder
+    // postorder
+// clone
+// mirror
+// delete:
+    // leaf node
+    // internal node
+    // intermediate node
+
+#include <iostream>
 using namespace std;
 
 class TreeNode {
-    public:
-        int data;
-        TreeNode *left, *right;
+public:
+    int data;
+    TreeNode *left, *right;
 
-        TreeNode(int value) : data(value), left(nullptr), right(nullptr) {}
+    TreeNode(int value) : data(value), left(nullptr), right(nullptr) {}
 };
 
 class Tree {
-    public:
-        TreeNode *root;
-        Tree(); 
-        void insert(int value);
-        void insert(TreeNode*& root, int value); 
-        void inOrder(TreeNode* root);
-        void create();  
+public:
+    TreeNode *root;
+    Tree();
+    void insert(int value);
+    void insert(TreeNode*& root, int value);
+    void inOrder(TreeNode* root);
+    void preOrder(TreeNode* root);
+    void postOrder(TreeNode* root);
+    void deleteNode(TreeNode* root, int key);
+    void create();
 };
 
-Tree::Tree() { 
-    root = nullptr; 
+Tree::Tree() {
+    root = nullptr;
 }
 
 void Tree::insert(TreeNode*& root, int value) {
@@ -45,6 +61,62 @@ void Tree::inOrder(TreeNode* root) {
     }
 }
 
+void Tree::preOrder(TreeNode* root) {
+    if (root != nullptr) {
+        cout << root->data << " ";
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+}
+
+void Tree::postOrder(TreeNode* root) {
+    if (root != nullptr) {
+        postOrder(root->left);
+        postOrder(root->right);
+        cout << root->data << " ";
+    }
+}
+
+void Tree::deleteNode(TreeNode* root, int key) {
+    TreeNode* temp = root;
+    TreeNode* temp1 = nullptr;
+
+    while (temp) {
+        if() {
+            if (temp->data == key) {
+                if (temp->left == nullptr && temp->right == nullptr) {
+                    if (temp1 == nullptr) {
+                        delete temp;
+                        this->root = nullptr;
+                    } 
+                    else {
+                        if (temp1->data > key) {
+                            temp1->left = nullptr;
+                        } 
+                        else {
+                            temp1->right = nullptr;
+                        }
+                        delete temp;
+                    }
+                    return;
+                }
+            } 
+            else if (key > temp->data) {
+                temp1 = temp;
+                temp = temp->right;
+            } 
+            else {
+                temp1 = temp;
+                temp = temp->left;
+            }
+        }
+        else if (){
+        
+        }   
+    }
+    
+}
+
 void Tree::create() {
     int choice, value;
     do {
@@ -52,24 +124,26 @@ void Tree::create() {
         cout << "1. Insert multiple nodes into the tree\n";
         cout << "2. Insert a single node into the tree\n";
         cout << "3. Display in-order traversal\n";
-        cout << "4. Exit\n";
+        cout << "4. Display pre-order traversal\n";
+        cout << "5. Display post-order traversal\n";
+        cout << "6. Delete a node from the tree\n";
+        cout << "7. Exit\n";
         cout << "Enter your choice: ";
         cin >> choice;
 
-        switch(choice) {
-            case 1:
-                {
-                    int n;
-                    cout << "How many nodes to insert? ";
-                    cin >> n;
-                    for (int i = 0; i < n; i++) {
-                        cout << "Enter value " << (i + 1) << ": ";
-                        cin >> value;
-                        insert(value);
-                    }
-                    cout << "Nodes inserted successfully.\n";
+        switch (choice) {
+            case 1: {
+                int n;
+                cout << "How many nodes to insert? ";
+                cin >> n;
+                for (int i = 0; i < n; i++) {
+                    cout << "Enter value " << (i + 1) << ": ";
+                    cin >> value;
+                    insert(value);
                 }
+                cout << "Nodes inserted successfully.\n";
                 break;
+            }
             case 2:
                 cout << "Enter value to insert: ";
                 cin >> value;
@@ -78,21 +152,37 @@ void Tree::create() {
                 break;
             case 3:
                 cout << "In-order traversal: ";
-                inOrder(root); 
+                inOrder(root);
                 cout << "\n";
                 break;
             case 4:
+                cout << "Pre-order traversal: ";
+                preOrder(root);
+                cout << "\n";
+                break;
+            case 5:
+                cout << "Post-order traversal: ";
+                postOrder(root);
+                cout << "\n";
+                break;
+            case 6:
+                cout << "Enter value to delete: ";
+                cin >> value;
+                deleteNode(root, value);
+                cout << "Node deleted successfully.\n";
+                break;
+            case 7:
                 cout << "Exiting program.\n";
                 break;
             default:
                 cout << "Invalid choice. Please try again.\n";
         }
-    } while(choice != 4); 
+    } while (choice != 7);
 }
 
 int main() {
     Tree tree;
-    tree.create();  
+    tree.create();
 
     return 0;
 }
